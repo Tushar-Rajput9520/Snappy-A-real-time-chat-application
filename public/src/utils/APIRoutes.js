ac79677677b1f8f4d8a5bbc99d28607d4669eaa2
Tushar Singh <tusharsinghkrj@gmail.com>
2025-08-29 09:24:54 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "snappy-a-real-time-chat-application-aipi-1dkmn2x87.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
